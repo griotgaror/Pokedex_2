81cd812edf971c09c459ec3cfa26d0754a49075f
griotgaror <martin.stiebing@web.de>
2022-04-07 02:45:58 +0200

    pokedex

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__, template_folder= "template");
 host = "0.0.0.0";
 port = 5050;
 
+
+def fetch_api(url):
+    response = requests.get(url);
+    return response.json();
+
+
 @app.route("/")
 def index():
-    return render_template("index.html");
+    limit = "20";
+    All_POKEMON = fetch_api("https://pokeapi.co/api/v2/pokemon/?limit=" + limit);
+
+    POKEMON = [];
+
+    for pokemon_data in All_POKEMON["results"]:
+        pokedata = fetch_api(pokemon_data["url"]);
+        
+        sprites = pokedata["sprites"];
+        types = pokedata["types"];
+
+        print(types[0]);
+
+        pokemon = {
+            "name": pokedata["name"],
+            "sprite_front": sprites["front_default"],
+            "sprite_back": sprites["back_default"],
+            "type": types[0]["type"]["name"],
+        }
+
+        POKEMON.append(pokemon);
+
+    return render_template("index.html", all_pokemon = POKEMON);
 
 
 if __name__ == "__main__":
--- static/index.js
+++ static/index.js
@@ -1,7 +1,40 @@
 
+var MAIN = {};
+MAIN.init = function() {
+	const POKEMON_CONTAINER = document.querySelectorAll(".pokemon_container");
 
-function main() {
-	alert("run");	
+	//spin card to the back or front side
+	POKEMON_CONTAINER.forEach(container => {
+		container.onclick = function() {
+
+			let back_side = "rotateY(90deg)";
+			let front_side = "rotateY(0deg)";
+
+			container.style.transform = (container.style.transform == front_side) ? back_side : front_side;  
+		};
+	});
+
+	//search pokemon
+	const SEARCHBAR = document.getElementById("searchbar");
+
+	SEARCHBAR.oninput = () => {
+		POKEMON_CONTAINER.forEach(container => {
+
+			let user_input = SEARCHBAR.value;
+			let pokename = container.id;
+
+			if (pokename.charAt(0) != user_input.charAt(0)) {
+				//show all pokemon it's the input none
+				container.style.display = (user_input) ? "none" : "flex";
+			} else {
+				//filter pokemon from user_input
+				for (let i = 1; i < user_input.length; i++) {
+					container.style.display = (pokename.charAt(i) == user_input.charAt(i)) ? "flex" : "none";
+				};
+			};
+		});
+	};
 };
 
-window.onload = main();
+
+window.onload = MAIN.init();
